f1650061907e73498acf944d884ffc5585577e80
Hoàng Nguyễn <87424201+qhoangfmsc@users.noreply.github.com>
2024-10-18 11:02:18 +0700

    Delete server directory

--- server/src/routes/public/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-// router.use("/accounts", users);
-import * as funcs from "../../controller/accountCon.js";
-
-const router = Router();
-router.post("/login", funcs.loginAccount);
-router.post("/create", funcs.createAccount);
-
-export default router;
