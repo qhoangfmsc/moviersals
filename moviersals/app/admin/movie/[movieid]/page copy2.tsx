f96324a2ff786863a3311134baf76d41b2e8869f
foxthehuman <foxthehuman1900@gmail.com>
2024-11-24 20:45:28 +0700

    revert movie detail admin page

--- moviersals/app/admin/movie/[movieid]/page copy2.tsx
+++ moviersals/app/admin/movie/[movieid]/page copy2.tsx
@@ -1,53 +1,77 @@
 "use client";
 
-import { CldUploadWidget } from "next-cloudinary";
+import getMovieDetailById from "@/api/movies/getMovieById";
+import AdminForm, { AdminFormCofig } from "@/components/Form/adminForm";
+import { title } from "@/components/primitives";
+import { BreadcrumbItem, Breadcrumbs, Button } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
-interface VideoUploaderProps {
-  folderName: string;
-  videoName: string;
-  onVideoUpload: (url: string) => void;
+interface MovieDetails {
+  movieid: string;
+  name: string;
 }
 
-export default function VideoUploader({ videoName, folderName, onVideoUpload }: VideoUploaderProps) {
-  const handleUploadResult = (error, result, widget) => {
-    if (result?.event === "success") {
-      onVideoUpload(result.info.secure_url);
-      widget.close(); // Close the widget after a successful upload
-    } else if (error) {
-      onVideoUpload("error");
-      console.error("Upload failed:", error);
-      widget.close(); // Close the widget in case of an error
-    }
+export default function MovieDetailAdminPage({
+  params,
+}: {
+  params: {
+    movieid: string;
+  };
+}) {
+  const [detailData, setData] = useState<MovieDetails>();
+
+  const fetchData = async () => {
+    const response = await getMovieDetailById(params.movieid);
+    const content = await response.content;
+    console.log(content);
+    setData(content);
+  };
+
+  useEffect(() => {
+    console.log("WAT", params);
+    fetchData();
+  }, []);
+
+  const adminFormCofig: AdminFormCofig = {
+    label: "Chỉnh sửa",
+    colList: [
+      { colname: "name", colsub: "Tên phim", coltype: "inputtext", colvalues: null },
+      { colname: "description", colsub: "Mô tả", coltype: "inputtext", colvalues: null },
+      { colname: "publisher", colsub: "Nhà sản xuất", coltype: "inputtext", colvalues: null },
+      { colname: "publishyear", colsub: "Năm sản xuất", coltype: "inputnumber", colvalues: null },
+      { colname: "thumbnail", colsub: "Ảnh bìa (đường dẫn)", coltype: "inputfile", colvalues: null },
+    ],
+    buttonText: "Xác nhận",
+    handler: async (request: { [key: string]: any }) => {
+      console.log(request);
+    },
   };
 
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <CldUploadWidget
-        // uploadPreset="your_upload_preset"
-        options={{
-          cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-          publicId: videoName,
-          folder: folderName,
-          uploadPreset: "default-preset",
-        }}
-        onSuccess={(result, widget) => handleUploadResult(null, result, widget)}
-        onError={(error, widget) => handleUploadResult(error, null, widget)}>
-        {({ open }) => (
-          <button
-            type="button"
-            onClick={() => open()}
-            style={{
-              backgroundColor: "#4CAF50",
-              color: "white",
-              padding: "10px 20px",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}>
-            Upload video
-          </button>
-        )}
-      </CldUploadWidget>
+    <div>
+      <h1 className={title()}>
+        Quản lý phim {detailData?.name} (#{detailData?.movieid})
+      </h1>
+      <Breadcrumbs
+        className="my-4"
+        itemClasses={{
+          item: "px-2",
+          separator: "px-0",
+        }}>
+        <BreadcrumbItem href="/admin">Moviersals</BreadcrumbItem>
+        <BreadcrumbItem href="/admin/movie">Phim ảnh</BreadcrumbItem>
+        <BreadcrumbItem href={`/admin/movie/${detailData?.movieid}`}>{detailData?.name}</BreadcrumbItem>
+      </Breadcrumbs>
+      <div className="flex lg:flex-row flex-col-reverse">
+        <div className="lg:w-4/5 lg:mr-8">
+          <div className="flex flexrow">
+            <Button color="success">Thêm mới tập phim</Button>
+          </div>
+        </div>
+        <div className="lg:w-1/5 mb-4">
+          <AdminForm adminFormCofig={adminFormCofig} />
+        </div>
+      </div>
     </div>
   );
 }
