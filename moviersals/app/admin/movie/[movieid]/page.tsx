d19f9bad608cf99c302b3cd5f223f975fa828349
Hoàng Nguyễn <nqh130901@gmail.com>
2024-11-24 20:22:14 +0700

    Update

--- moviersals/app/admin/movie/[movieid]/page.tsx
+++ moviersals/app/admin/movie/[movieid]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import getMovieById from "@/api/movies/getMovieById";
+import AddNewEpisodeAdminForm from "@/components/Episode/episodeAdminForm";
 import AdminForm, { AdminFormCofig } from "@/components/Form/adminForm";
 import { title } from "@/components/primitives";
 import { BreadcrumbItem, Breadcrumbs, Button } from "@nextui-org/react";
@@ -13,7 +14,7 @@ export default function MovieDetailAdminPage({
     movieid: string,
   }
 }) {
-  const [detailData, setData] = useState([])
+  const [detailData, setData] = useState(null)
   useEffect(() => {
     const fetchData = async () => {
       const response = await getMovieById(params.movieid);
@@ -66,7 +67,7 @@ export default function MovieDetailAdminPage({
 
   return (
     <div>
-      <h1 className={title()}>Quản lý phim {detailData.name} (#{detailData.movieid})</h1>
+      <h1 className={title()}>Quản lý phim {detailData?.name} (#{detailData?.movieid})</h1>
       <Breadcrumbs
         className="my-4"
         itemClasses={{
@@ -76,12 +77,12 @@ export default function MovieDetailAdminPage({
       >
         <BreadcrumbItem href="/admin">Moviersals</BreadcrumbItem>
         <BreadcrumbItem href="/admin/movie">Phim ảnh</BreadcrumbItem>
-        <BreadcrumbItem href={`/admin/movie/${detailData.movieid}`}>{detailData.name}</BreadcrumbItem>
+        <BreadcrumbItem href={`/admin/movie/${detailData?.movieid}`}>{detailData?.name}</BreadcrumbItem>
       </Breadcrumbs>
       <div className="flex lg:flex-row flex-col-reverse">
         <div className="lg:w-4/5 lg:mr-8">
           <div className="flex flexrow">
-            <Button color="success">Thêm mới tập phim</Button>
+            <AddNewEpisodeAdminForm />
           </div>
         </div>
         <div className="lg:w-1/5 mb-4">
---
+++ moviersals/components/Episode/episodeAdminForm.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { FormEvent, useState } from "react";
+import { useParams } from "next/navigation";
+import uploadEpisode from "@/api/episode/uploadEpisode";
+
+export default function AddNewEpisodeAdminForm() {
+    const params = useParams();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const handleWheel = (e: React.WheelEvent<HTMLInputElement>) => {
+        (e.target as HTMLInputElement).blur();
+    };
+
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const formData = new FormData(event.currentTarget);
+            const episodename = formData.get("name")?.toString();
+            const episodepath = formData.get("episodepath")?.toString();
+            const episodenumber = formData.get("episodenumber")?.toString();
+            const movieid = params?.movieid as string;
+
+            console.log(episodename, episodepath, episodenumber, movieid);
+
+            if (episodename && episodepath && episodenumber && movieid) {
+                const response = await uploadEpisode(movieid, episodename, episodepath, episodenumber);
+                setIsLoading(false);
+                if (response.result == "success") {
+                    setIsLoading(false);
+                } else {
+                    setIsLoading(false);
+                    setError(response.content);
+                }
+            } else {
+                setIsLoading(false);
+                setError("Vui lòng nhập đầy đủ thông tin!");
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                setIsLoading(false);
+                setError(error.message);
+            }
+        }
+    }
+
+    return (
+        <form
+            className="flex flex-col my-8 items-left border border-gray-300 p-8 rounded-lg w-fit"
+            onSubmit={onSubmit}>
+            {error && <div style={{ color: "red" }}>{error}</div>}
+            <Input
+                size="lg"
+                className="max-w-[350px]"
+                type="text"
+                name="name"
+                variant="underlined"
+                label="Tên tập phim"
+            />
+            <Input
+                size="lg"
+                className="max-w-[350px]"
+                type="text"
+                name="episodepath"
+                variant="underlined"
+                label="Đường dẫn tập phim"
+            />
+            <Input
+                size="lg"
+                className="max-w-[350px]"
+                type="number"
+                onWheel={(e) => handleWheel(e)}
+                name="episodenumber"
+                variant="underlined"
+                label="Tập phim thứ"
+            />
+            <Button
+                size="lg"
+                className="mt-8 mb-4 w-[350px]"
+                type="submit"
+                disabled={isLoading}
+                variant="shadow"
+                color="success">
+                {isLoading ? "Loading..." : "Upload tập phim"}
+            </Button>
+        </form>
+    );
+}
