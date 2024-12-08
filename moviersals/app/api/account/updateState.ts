f7a11e82e0fa0257abbe0d50a654fdc5cb6fa682
foxthehuman <foxthehuman1900@gmail.com>
2024-12-08 16:43:56 +0700

    fix admin ticket, episode, user

--- moviersals/app/api/account/updateState.ts
+++ moviersals/app/api/account/updateState.ts
@@ -2,7 +2,7 @@ import { requestApi } from "@/lib/requestApi";
 
 export default async function updateState(request : Object) {
   const url = "/api/internal/account/updateState";
-  const res = await requestApi(url, request);
+  return await requestApi(url, request);
 
-  return (typeof res?.content == "object") ? res?.content : null;
+  // return (typeof res?.content == "object") ? res?.content : null;
 }
