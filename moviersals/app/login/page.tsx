422d99c5be9f5e7cfc68f79ab15e833b11b10d2a
foxthehuman <foxthehuman1900@gmail.com>
2024-12-10 18:57:00 +0700

    Update page.tsx

--- moviersals/app/login/page.tsx
+++ moviersals/app/login/page.tsx
@@ -1,16 +1,22 @@
-"use client"
+"use client";
 
+import FallbackDetail from "@/components/Fallback/fallbackDetail";
 import LoginForm from "@/components/Form/loginForm";
 import Transition from "@/components/MotionFramer/transition";
 import { title } from "@/components/primitives";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <Transition>
-      <div>
-        <h1 className={title()}>Chào mừng đến với <b>Moviersals</b></h1>
-        <LoginForm />
-      </div>
+      <Suspense fallback={<FallbackDetail />}>
+        <div>
+          <h1 className={title()}>
+            Chào mừng đến với <b>Moviersals</b>
+          </h1>
+          <LoginForm />
+        </div>
+      </Suspense>
     </Transition>
   );
 }
